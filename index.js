bfba5ded77483a0017772dc1b335b3b37f5b3849
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-09 15:19:17 +0100

    Numeri nascosti, compare input per utente

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ for(let i=0; i<5; i++){
     let numero = Math.trunc(Math.random() * 10 + 1);
     divNumeri.append(numero + " ");
 }
+let x = 0;
 function nascondi_numeri(){
-    
+    divNumeri.innerHTML = "";
+    let input = document.createElement("input");
+    divNumeri.appendChild(input);
+    let bottone = document.createElement("button");
+    bottone.textContent = "Verifica";
+    bottone.addEventListener("click", function(){
+        x++;
+        if(x<=5){
+            numeriUtente.push(input.value);
+            console.log(numeriUtente);
+        }
+    });
+    divNumeri.appendChild(bottone);
+    let numeriUtente = [];
 }
 let nascondiNumeri = setTimeout(nascondi_numeri, 30000);
